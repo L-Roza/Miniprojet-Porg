c0f2270b8c3ebd49902fc6c65c2aa69c82a9c85c
L-Roza <roza.lachi@ummto.dz>
2025-12-02 23:57:52 +0100

    Update test_anomlaly.py

--- tests/test_anomlaly.py
+++ tests/test_anomlaly.py
@@ -1,6 +1,6 @@
 from analytics.anomaly_detector import AnomalyDetector
-
 def test_anomaly():
     detector = AnomalyDetector(100)
-    assert detector.is_anomalous(120) == True
-    assert detector.is_anomalous(80) == False
+    assert detector.is_anomalous(120) is True
+    assert detector.is_anomalous(80) is False
+
